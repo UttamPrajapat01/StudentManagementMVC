e908287a5f17026468723a09c5d3025d1ebc1e88
Uttam Prajapat <uttam@evisionits.com>
2026-05-11 09:34:09 +0530

    Add missing line in migration designer file

--- StudentManagementMVC/Migrations/20260120051948_createtable.Designer.cs
+++ StudentManagementMVC/Migrations/20260120051948_createtable.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
-// ✅ ADD THIS LINE
+
 using StudentManagementMVC.Data;
 
 #nullable disable
